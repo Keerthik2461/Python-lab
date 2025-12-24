7452f0cf08d635ee259e5890d9e128ebde011ca4
Keerthik2461 <keerthikraja58@gmail.com>
2025-12-24 15:48:31 +0530

    Vote

---
+++ sample.py
@@ -0,0 +1,6 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+if(age>=18):
+    print("Hello, " + name + "! You are eligible to vote.")
+else:
+    print("Hello, " + name + "! You are not eligible to vote.")
