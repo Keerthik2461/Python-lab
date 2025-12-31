f351e7593cc3a2e0c6d4ea6cfbd0932e182f43c5
Keerthik2461 <keerthikraja58@gmail.com>
2025-12-31 15:16:03 +0530

    Candidate Algorithm

--- cea.py
+++ cea.py
@@ -57,7 +57,7 @@ training_data = [
     ['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'Yes'],
     ['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'Yes'],
     ['Rainy', 'Cold', 'High', 'Strong', 'Warm', 'Change', 'No'],
-    ['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'Yes']
+  
 ]
 
 # Run algorithm
